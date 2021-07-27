195ca0c181f49c8883391224b79a3f2071c26fac
Revivedaniel <mlg.danielstark@gmail.com>
2021-07-27 14:00:00 -0700

    Implimented the google geocoding api and began setting up JSON

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,48 @@
+//initializing local storage
+var listOfCities = {}
+//validating if there is an item in local storage with the key listOfCities
+if (localStorage.getItem("listOfCities")) {
+  //set the listOfCities variable to the value from local storage
+  listOfCities = JSON.parse(localStorage.getItem("listOfCities"));
+  //set the city selected item in local storage to the first city in listOfCities' locationName
+  localStorage.setItem("citySelected", listOfCities[0].locationName)
+} else {
+  listOfCities = {
+    LaHabra: {
+      locationName: "LaHabra",
+      lat: 33.9318591,
+      lng: -117.946137
+    }
+  }
+  localStorage.setItem("listOfCities", JSON.stringify(listOfCities))
+}
+//variable for city selected
+var citySelected = localStorage.getItem("citySelected");
+//slecting the current weather article
+var currentWeather = document.querySelector("#currentWeather");
+//selecting all the children of current weather
+var currentWeatherChildren = currentWeather.children;
+//Selecting the search area
+var searchCityEl = document.querySelector("#searchCity");
+//submit button
+var submitBtnEl = document.querySelector("#submitButton")
+//begining of api
+const beginAPI = "https://maps.googleapis.com/maps/api/geocode/json?"
+const apiKey = "&key=AIzaSyAjHWedDwUnS1H21XfbW-e388yuURcoWU0"
+
+var latLng = {
+  lat: 0,
+  lng: 0,
+}
+
 //when the search button is pressed
+submitBtnEl.addEventListener("click", function(event) {
   //prevent default
+  event.preventDefault();
+  //search the value in geocode api
+
+})
   //search the value in the weather api
-    //if the curl is successful and there isnt an entry in the local storage for that city
       //set a selection item in localStorage to the city
       //add a item to the local storage array
       //create a new li at the bottom
@@ -11,22 +52,32 @@
 //on load
   //check the selection item in localStorage
   //if it is blank
-    //display la habra by default
-var citySelected = localStorage.getItem("citySelected") || "La Habra";
+    //set to La Habra as default
+if (!localStorage.getItem("citySelected")) {
+  localStorage.setItem("citySelected", "LaHabra");
+  citySelected = "LaHabra"
+}
 
-//slecting the current weather article
-var currentWeather = document.querySelector("#currentWeather");
-//selecting all the children of current weather
-var currentWeatherChildren = currentWeather.children;
-//Selecting the search area
-var searchCityEl = document.querySelector("#searchCity");
-
-
-
-    //if it is populated
       //fill in all information in the current day section
       //fill in the 5-day forcast
 
+function callGeolocation() {
+  fetch(beginAPI + searchCityEl.value.replace(/\s+/g, '') + apiKey)
+    .then(function(response) {
+      if (response === 200) {
+        //if the curl is successful and there isnt an entry in the local storage for that city
+        if (condition) {
+          
+        }
+
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      latLng.lat = data.geometry.location.lat;
+      latLng.lng = data.geometry.location.lng;
+    })
+}
 
 
 //populate current day function
